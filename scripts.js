c0fce2f68e7bb306de161127c5bd8348f58c8d1d
nishaunlawrence <nishaun.lawrence@gmail.com>
2022-10-30 16:50:23 -0500

    Final Submission

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+window.addEventListener('load', function(){
+    var submit = document.querySelector(".newsletter button");
+    var message = document.querySelector(".message");
+    const email = document.getElementById("email");
+    var reGex = /^\S+@\S+\.\S+$/i;
+
+    email.textContent = "";
+
+
+    submit.addEventListener('click', function(event){
+        event.preventDefault();
+        var userEmail = email.value;
+
+        if(userEmail === "" || !reGex.test(userEmail)) {
+
+            message.textContent = "Please enter a valid email address";
+        }else{
+            message.textContent = `Thank You! Your email address ${userEmail} has been added to our mailing list!`;
+        }
+    });
+}
+});
